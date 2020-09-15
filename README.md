0206175d467dcc3ed3f4a9e611e74687b3bb307f
Fer Benavides <nockzblack@gmail.com>
2020-09-15 12:27:17 -0500

    mongodb drived implemented
    Signed-off-by: Fer Benavides <nockzblack@gmail.com>

--- README.md
+++ README.md
@@ -2,12 +2,16 @@ Installation
 
 1. Install MongoDB depending on your OS
 
+For docker run on command line:
+get docker
+docker pull mongo
+docker run --name devxican -P -d mongo
+
 https://docs.nodebb.org
 
 Direct dependencies used:
+
 - express-generator (https://www.npmjs.com/package/express-generator)
-with the next flags: express --view=ejs
+  with the next flags: express --view=ejs
 
 - mongodb driver (https://www.npmjs.com/package/mongodb)
-
-
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/db", (req, res, next) => {
+  let db = req.app.get("db");
+  console.log(db);
+  res.send("Request recived");
 });
 
 module.exports = router;
