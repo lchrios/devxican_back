1164c2851952d6be5452ee46b0ab2a9f31461c34
Fer Benavides <nockzblack@gmail.com>
2020-09-23 12:00:55 -0500

    Post an answer from given question id
    Signed-off-by: Fer Benavides <nockzblack@gmail.com>

--- tools/main.js
+++ tools/main.js
@@ -1,6 +1,6 @@
 var Cliente = require("./cliente.js");
 
-var auxCliente = new Cliente("localhost", "8080", "http");
+var auxCliente = new Cliente("localhost", "9999", "http");
 
 let auxPost = {
   title: Math.random()
@@ -16,9 +16,22 @@ let auxPost = {
   dislikes: Math.floor(Math.random() * 100),
 };
 
-auxCliente.post("/question/new", auxPost, (res) => {
+let auxComment = {
+  date: new Date(),
+  author: "Some Author",
+  comment:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas convallis libero nec mauris sodales, at laoreet ligula efficitur. Ut et tincidunt lacus, ut cursus ex. Phasellus ornare nibh at nisl tincidunt sagittis. Aliquam erat volutpat. Curabitur efficitur diam nec scelerisque venenatis. Donec accumsan lacinia justo et mattis. Maecenas eu faucibus odio. Morbi venenatis dui turpis, vel fermentum lacus vehicula id. Suspendisse potenti.",
+  likes: Math.floor(Math.random() * 100),
+  dislikes: Math.floor(Math.random() * 100),
+};
+
+auxCliente.post("/questions/5f6109f2fb6bc302f3742bd8/comments", auxComment, (res) => {
   console.log(res);
 });
+
+//auxCliente.post("/questions", auxPost, (res) => {
+//  console.log(res);
+//});
 
 /*
 var clienteGitHub = new Cliente("api.github.com", 443, "https");
